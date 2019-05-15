8a9cbf9c37bf4a6156d8f27ac21f0e83a86f1035
SFin94 <smf115@ic.ac.uk>
2019-05-15 14:55:14 +0200

    Working version for tutorial molecule; fixed planar neighbour problem

--- waterCoords.py
+++ waterCoords.py
@@ -5,6 +5,22 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
+# GS process
+def gsProcess(xVec, *bVecs):
+
+    '''Function which calculates a new orthogonal basis vector by the Gram Shcmidt process'''
+
+    sum = 0
+    for bVec in bVecs:
+        sum += (np.dot(xVec, bVec)/np.linalg.norm(bVec))*bVec
+    bNew = xVec - sum
+    return(bNew)
+
+def tripleProd(nBonds):
+    nBcross = np.cross(nBonds[0], nBonds[1])
+    nBtripleProd = np.dot(nBonds[2], nBcross)
+    return(nBtripleProd)
+
 class waterPosition:
 
     # Set geometry constants for TIP3P water molecule
@@ -41,16 +57,6 @@ class waterPosition:
         for nB in neighbourInds:
             self.neighbours.append(self.geometry[int(nB) - 1])
 
-    # GS process
-    def gsProcess(xVec, *bVecs):
-
-        '''Function which calculates a new orthogonal basis vector by the Gram Shcmidt process'''
-
-        sum = 0
-        for bVec in bVecs:
-            sum += (np.dot(xVec, bVec)/np.linalg.norm(bVec))*bVec
-        bNew = xVec - sum
-        return(bNew)
 
     # Rotation around y axis
     def rotationY(self, feta, inVec, bOne = np.array([1., 0., 0.]), bTwo = np.array([0., 1., 0.]), bThree = np.array([0., 1., 0.])):
@@ -75,74 +81,6 @@ class waterPosition:
 
         return(outVec)
 
-
-    def donorSetUp(self):
-
-        '''Function to set up water molecule with donor interaction - updates the water and dummy atom positons for the object
-
-        Parameters:
-         donMol - numpy array (dim: 1x3): x, y, z coordinates of the donor atom in the molecule
-         neighbour - numpy array (dim: 1x3): x, y, z coordinates of the neighbouring atom to the H donor atom in the molecule (should only be one as donor always H?)
-        '''
-
-        # Calculate vector b1 using the donor and H positions, use to calculate O position
-        b1 = self.targMol - self.neighbours[0]
-        b1 /= np.linalg.norm(b1)
-        self.OW = self.targMol + b1*2
-
-        # Find orthonormal basis with b1 using cross products (GS process alterantive works but was unnecessary as orthogonal basis already created)
-        b2 = np.cross(self.targMol, self.neighbours[0])
-        b2 /= np.linalg.norm(b2)
-        b3 = np.cross(b2, b1)
-        b3 /= np.linalg.norm(b3)
-
-        # Define dummy atoms for angles from the H donor
-        self.dOne = self.targMol + b2
-        self.dTwo = self.targMol + b3
-
-        # Rotate standard basis by desired angles around the y axis
-        rotOne = self.rotationY(self.angles[0], np.array([1., 0., 0.]))
-        rotTwo = self.rotationY(self.angles[1], np.array([1., 0., 0.]))
-
-        # Construct transition matrix from standard basis to donor basis (inv is transpose). Order is matched to rotation done (would be rotating b2 around b3); making them b1 and b2 respectively
-        bPx = np.array([b2, b3, b1]).transpose()
-        # Transform the rotation vectors for the water H's to the donor basis, scale, and add to the water O
-        self.HWOne = np.matmul(bPx, rotOne)*self.bondOH + self.OW
-        self.HWTwo = np.matmul(bPx, rotTwo)*self.bondOH + self.OW
-
-
-#    def acceptorCalc(self, b1):
-#
-#        '''Function which calculates the position of the water molecule for an inputted position of the acceptor atom and b1 vector, updating the positions of the water and dummy atoms.
-#
-#            Parameters:
-#             accMol- numpy array (dim: 1x3): x, y, z coordinates of the acceptor atom
-#             b1Pos - numpy array (dim: 1x3): x, y, z coordinates of the b1 site of the acceptor atom
-#        '''
-#
-#        self.HWOne = self.targMol + b1*2
-#        # Position O bond distance away from the H
-#        self.OW = self.HWOne + b1*self.bondOH
-#
-#        # Position the two dummy atoms
-#        # Find orthonormal basis with b1 using cross products (GS process alternative works but was unnecessary as orthogonal basis already created)
-#        b2 = np.cross(self.targMol, b1)
-#        b2 /= np.linalg.norm(b2)
-#        b3 = np.cross(b2, b1)
-#        b3 /= np.linalg.norm(b3)
-#
-#        # Define dummy atoms for angles from the H donor
-#        self.dOne = self.targMol + b2
-#        self.dTwo = self.targMol + b3
-#
-#        # For second OH bond the angle will be angleHOH - 90
-#        rot = self.rotationY(-(self.angleHOH - 0.5*np.pi), np.array([1., 0., 0.]))
-#        # Construct transition matrix from standard basis to donor basis (inv is transpose). Order is matched to rotation done (would be rotating b2 around b3); making them b1 and b2 respectively
-#        bPx = np.array([b2, b3, b1]).transpose()
-#        # Transform the rotation vectors for the second water H to the donor basis, scale, and add to the water O
-#        self.HWTwo = np.matmul(bPx, rot)*self.bondOH + self.OW
-
-
     def waterSetUp(self, target):
 
         '''Function to set up water molecule with acceptor interaction - calculates the b1 vector dependant on the number of covalently bonded neighbours and updates the water and dummy atom positons for the object through the acceptorCalc function
@@ -158,19 +96,27 @@ class waterPosition:
             cInitial = nB - self.targMol
             cBonds.append(cInitial)
 
-        # Find centroid of the bonds - centre of mass?- could be same as case for 2?
+        # Find centroid of the bonds and normalise
         b1 = np.array([0., 0., 0.])
         for cB in cBonds:
             b1 += cB
         b1 /= len(cBonds)
         b1 /= np.linalg.norm(b1)
 
-        # Position the two dummy atoms
         # Find orthonormal basis with b1 using cross products (GS process alternative works but was unnecessary as orthogonal basis already created)
         b2 = np.cross(self.targMol, b1)
         b2 /= np.linalg.norm(b2)
         b3 = np.cross(b2, b1)
         b3 /= np.linalg.norm(b3)
+
+#         Have that b2 or b3 would be the ones orthogonal to the bonding plane - would want to set that one as b1
+#         Test triple product; if close to 0 then in same plane
+        if len(cBonds) == 3:
+            if tripleProd(cBonds) < 1e-03:
+                if abs(tripleProd([cBonds[0], cBonds[1], b2])) < 1e-03:
+                    b3, b1 = b1, b3
+                elif abs(tripleProd([cBonds[0], cBonds[1], b3])) < 1e-03:
+                    b2, b1 = b1, b2
 
         # Define dummy atoms for angles from the H donor
         self.dOne = self.targMol + b2
@@ -178,7 +124,7 @@ class waterPosition:
 
         if target == 'don':
 
-            self.OW = self.targMol + b1*2
+            self.OW = self.targMol - b1*2
 
             # Rotate standard basis by desired angles around the y axis
             rotOne = self.rotationY(self.angles[0], np.array([1., 0., 0.]))
@@ -206,10 +152,6 @@ class waterPosition:
             self.dThree = self.HWOne + b2
 
 
-    def idealisedInt(self, target):
-
-        # Only calculate for
-
 
     def calcZMat(self, target):
 
@@ -235,22 +177,32 @@ class waterPosition:
 
         elif target == 'acc':
 
+            #dThreezMat = {'x3': numAtoms+2, 'x1': 2.00, self.targMolID: 90.00, 'x2': -90.00}
             # Trial one - add third dummy atom
             HWOnezMat = {'H1w': numAtoms+3, self.targMolID: 'rAH', 'x1': 'HAx1', 'x2': 'HAx1x2'}
 
+# Add dThree as second one and not defined by coordinates?
+
             ang = gg.atomAngle(self.OW, self.HWOne, self.dThree)
             dihed = gg.atomDihedral(self.OW, self.HWOne, self.dThree, self.targMol)
-            OWzMat = {'Ow': numAtoms+5, 'H1w': self.bondOH, 'x3': ang, self.targMolID: dihed}
+#            OWzMat = {'Ow': numAtoms+5, 'H1w': self.bondOH, 'x3': ang, self.targMolID: dihed}
+            dist = gg.atomDist(self.OW, self.dThree)
+            ang = gg.atomAngle(self.OW, self.dThree, self.HWOne)
+            dihed = gg.atomDihedral(self.OW, self.dThree, self.HWOne, self.targMol)
+            OWzMat = {'Ow': numAtoms+4, 'x3': dist, 'H1w': ang, self.targMolID: dihed}
+
 
             ang = gg.atomAngle(self.HWTwo, self.OW, self.targMol)
-            HWTwozMat = {'H2w': numAtoms+6, 'Ow': self.bondOH, self.targMolID: ang, 'x2': 'HOHA'}
+            HWTwozMat = {'H2w': numAtoms+5, 'Ow': self.bondOH, self.targMolID: ang, 'x2': 'HOAx'}
 
             # Start value for one dihedral seems to be out so calculate?
-            HOHAInit = gg.atomDihedral(self.HWTwo, self.OW, self.targMol, self.dTwo)
-            optVar = {'rAH': 2.00, 'HAx1': 90.0, 'HAx1x2': -90.0, 'HOHA': HOHAInit}
-
+            HAx1Init = gg.atomAngle(self.HWOne, self.targMol, self.dOne)
+            HAx1x2Init = gg.atomDihedral(self.HWOne, self.targMol, self.dOne, self.dTwo)
+            HOAxInit = gg.atomDihedral(self.HWTwo, self.OW, self.targMol, self.dTwo)
+            optVar = {'rAH': 2.00, 'HAx1': HAx1Init, 'HAx1x2': HAx1x2Init, 'HOAx': HOAxInit}
 
             zMatList = [HWOnezMat, OWzMat, HWTwozMat]
+            #zMatList = [dThreezMat, HWOnezMat, OWzMat, HWTwozMat]
 
         with open('{}Int{}_ZMat.com'.format(target, self.targMolID), 'w') as output:
             print('%Chk={}Int{}_Zmat'.format(target, self.targMolID), file=output)
@@ -265,7 +217,8 @@ class waterPosition:
             # Print both dummy atoms - don't need to be ZMat and should maintain consistency
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x1', self.dOne[:]), file=output)
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x2', self.dTwo[:]), file=output)
-            print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x3', self.dThree[:]), file=output)
+            if target == 'acc':
+                print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x3', self.dThree[:]), file=output)
             # Set z matrix input
             for atom in zMatList:
                 zMatInput = '{:<4}'.format(list(atom.keys())[0])
@@ -298,6 +251,8 @@ class waterPosition:
                 print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format((self.atomIds[atomInd]), el[:]), file=output)
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x1', self.dOne[:]), file=output)
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x2', self.dTwo[:]), file=output)
+            if target == 'acc':
+                print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x3', self.dThree[:]), file=output)
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('Ow', self.OW[:]), file=output)
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('H1w', self.HWOne[:]), file=output)
             print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('H2w', self.HWTwo[:]), file=output)
