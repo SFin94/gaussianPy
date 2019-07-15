2ec92b5ef8b01d84a25d1f3c243828e95137c5c5
SFin94 <smf115@ic.ac.uk>
2019-07-15 16:02:17 +0100

    Created input parser to handle acc arguments a bit neater; merged writing functions to one

--- waterCoords.py
+++ waterCoords.py
@@ -6,7 +6,7 @@ import gaussGeom as gg
 # GS process
 def gramScmidt(xVec, *bVecs):
 
-    '''Function which calculates a new orthogonal basis vector by the Gram Shcmidt process'''
+    '''Function which calculates a new orthogonal basis vector by the Gram Schmidt process'''
 
     sum = 0
     for bVec in bVecs:
@@ -89,12 +89,11 @@ class InteractionSite:
     bondOH = 0.9572
     angleHOH = np.radians(104.52)
 
-    def __init__(self, raw, extraID=''):
+    def __init__(self, raw):
 
         # Set the id index of the target mol
         self.atomID = raw[0] + raw[1]
         self.atomInd = int(raw[1])
-        self.fileID = extraID
 
         # Set the list of covalently bonded neighbours
         self.neighbourInd = []
@@ -103,12 +102,6 @@ class InteractionSite:
 
         # Assign the type of site to be set up
         self.siteType = str(raw[3])
-
-        if len(raw) > 4:
-            self.lonePairs = int(raw[4])
-        else:
-            self.lonePairs = 0
-
 
     def localGeom(self, geometry):
 
@@ -189,18 +182,26 @@ class InteractionSite:
         self.dummyAtoms = self.dummySetUp()
 
 
-    def writeZMat(self, geometry, atomIDs, fileName='zMat'):
+    def writeOutput(self, geometry, atomIDs, format='zMat', extraID=''):
 
         ''' Function which writes a gaussian input file with the constrained optimisation input in z matrix form
 
             Inputs:
-            geometry: x, y, z coordinates of each atom in the molecule
-            atomIDs: List of atomIDs in the molecule
-            name: str - optional add on identifier for the input file (default: 'zMat')
-            '''
+             geometry: x, y, z coordinates of each atom in the molecule
+             atomIDs: List of atomIDs in the molecule
+             name: str - optional add on identifier for the input file (default: 'zMat')
+        '''
 
-        with open('{}Int{}_{}.com'.format(self.siteType, self.atomID, fileName), 'w') as output:
-            print('%Chk={}Int{}_{}'.format(self.siteType, self.atomID, fileName), file=output)
+        # Set name for file - need unique ID
+        fileID = self.atomID + extraID
+        if self.siteType == 'acc':
+            if self.nNeighbours == 4:
+                fileID += '_' + str(self.neighbourInd[0])
+        elif self.inv == True:
+            fileID += '_inv'
+
+        with open('{}Int{}_{}.com'.format(self.siteType, fileID, format), 'w') as output:
+            print('%Chk={}Int{}_{}'.format(self.siteType, fileID, format), file=output)
             print('%NProcShared=12', file=output)
             print('%Mem=46000MB', file=output)
             print('#P HF/6-31G(d) Opt(Z-Matrix,MaxCycles=100) Geom(PrintInputOrient) SCF(Conver=9) Int(Grid=UltraFine)\n', file=output)
@@ -212,55 +213,29 @@ class InteractionSite:
             # Print dummy atom coordinates
             for dInd, dAtom in enumerate(self.dummyAtoms):
                 print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x' + str(dInd+1), dAtom[:]), file=output)
+
             # Process z matrix into string and print for water position
-            for waterAtom in self.zMatList:
-                zMatInput = '{:<4}'.format(list(waterAtom.keys())[0])
-                for entry in list(waterAtom.items())[1:]:
-                    if isinstance(entry[1], str):
-                        zMatInput += '{:>4}{: >8}'.format(entry[0], entry[1])
-                    else:
-                        zMatInput += '{:>4}{: >8.2f}'.format(entry[0], entry[1])
-                print(zMatInput, file=output)
-            # Print out the variables section
-            print('', file=output)
-            # Enter initial variables
-            for var, inVal in self.optVar.items():
-                print('{:<8}{:>6.2f}'.format(var, inVal), file=output)
+            if 'zMat' in format:
+                for waterAtom in self.zMatList:
+                    zMatInput = '{:<4}'.format(list(waterAtom.keys())[0])
+                    for entry in list(waterAtom.items())[1:]:
+                        if isinstance(entry[1], str):
+                            zMatInput += '{:>4}{: >8}'.format(entry[0], entry[1])
+                        else:
+                            zMatInput += '{:>4}{: >8.2f}'.format(entry[0], entry[1])
+                    print(zMatInput, file=output)
+                # Print out the variables section
+                print('', file=output)
+                # Enter initial variables
+                for var, inVal in self.optVar.items():
+                    print('{:<8}{:>6.2f}'.format(var, inVal), file=output)
+
+            # Write in coordinates format
+            else:
+                for waterAtom in zip(['Ow', 'H1w', 'H2w'],[self.waterPos[1], self.waterPos[0], self.waterPos[2]]):
+                    print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format(waterAtom[0], waterAtom[1][:]), file=output)
+
             print('\n\n', file=output)
-
-
-    def writeCoords(self, geometry, atomIDs, fileName='coords'):
-
-        ''' Function which writes a gaussian input file with just the cartesian coordinates of the molecule and water
-
-            Inputs:
-             geometry: x, y, z coordinates of each atom in the molecule
-             atomIDs: List of atomIDs in the molecule
-             name: str - optional add on identifier for the input file (default: 'zMat')
-        '''
-
-        with open('{}Int{}_{}.com'.format(self.siteType, self.atomID, fileName), 'w') as output:
-            print('%Chk={}Int{}_{}'.format(self.siteType, self.atomID, fileName), file=output)
-            print('%NProcShared=24', file=output)
-            print('%Mem=61000MB', file=output)
-            print('#P HF/6-31G(d) Opt(MaxCycles=100) Geom(PrintInputOrient) SCF(Conver=9) Int(Grid=UltraFine)\n', file=output)
-            print('Water {} interaction for {}\n'.format(self.siteType, self.atomID), file=output)
-            print('0 1', file=output)
-            # Print original molecular geometry
-            for atomInd, el in enumerate(geometry):
-                print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format((atomIDs[atomInd]), el[:]), file=output)
-            for dInd, dAtom in enumerate(self.dummyAtoms):
-                print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format('x' + str(dInd+1), dAtom[:]), file=output)
-            for waterAtom in zip(['Ow', 'H1w', 'H2w'],[self.waterPos[1], self.waterPos[0], self.waterPos[2]]):
-                print('{0:<4} {1[0]: >10f} {1[1]: >10f} {1[2]: >10f}'.format(waterAtom[0], waterAtom[1][:]), file=output)
-            print('\n\n', file=output)
-
-    def zMatWriter(self, geometry, atomIDs):
-
-        self.idealzMat()
-        self.writeZMat(geometry, atomIDs, fileName='idealzMat' + self.fileID)
-        site.maxIntzMat()
-        self.writeZMat(geometry, atomIDs, fileName='maxIntzMat' + self.fileID)
 
 
 class DonorInt(InteractionSite):
@@ -269,9 +244,9 @@ class DonorInt(InteractionSite):
         Sets variables and water position for a donor interaction
     '''
 
-    def __init__(self, raw, extraID=''):
+    def __init__(self, raw):
 
-        InteractionSite.__init__(self, raw, extraID)
+        InteractionSite.__init__(self, raw)
 
         # Calculate angles for rotation from basis to H positions and peform rotation for O-H bond vectors
         angles = [-(np.pi-self.angleHOH)/2, -(np.pi+self.angleHOH)/2]
@@ -339,9 +314,15 @@ class AcceptorInt(InteractionSite):
         Sets variables and water position for an acceptor interaction
         '''
 
-    def __init__(self, raw, extraID=''):
+    def __init__(self, raw, nNeighbours=None, lp=0, inv=False):
 
-        InteractionSite.__init__(self, raw, extraID)
+        # Instantiate parent class
+        InteractionSite.__init__(self, raw)
+
+        if nNeighbours == None:
+            self.nNeighbours = len(self.neighbourInd)
+        else:
+            self.nNeighbours = nNeighbours
 
         # Calculate water geometry w.r.t standard basis and origin as acceptor atom
         rot = totalRot(0, -(self.angleHOH - 0.5*np.pi), 0, np.array([1., 0., 0.]))
@@ -349,6 +330,11 @@ class AcceptorInt(InteractionSite):
         O = H1 + np.array([0, 0, self.bondOH])
         H2 = O + rot*self.bondOH
         self.waterGeom = np.array([H1, O, H2])
+
+        # Set additional acceptor type attributes
+        self.lonePairs = lp
+        self.inverse = inv
+
 
     def dummySetUp(self):
 
@@ -408,6 +394,43 @@ class AcceptorInt(InteractionSite):
         self.zMatList = [waterH1zMat, waterOzMat, waterH2zMat]
 
 
+def inputParse(input):
+    # Create an acceptor/donor interactionSite object for each water interaction site
+
+    # Empty list for interaction site object
+    siteList = []
+
+    for el in input:
+        # Skip line if comment
+        if el[0] != '#':
+            # If donor then doesn't need anymore processing
+            if el.split()[3] == 'don':
+                siteList.append(DonorInt(el.split()))
+
+            elif el.split()[3] == 'acc':
+
+                # If 4 neighbours then have to instantiate for each set of three and inverse set
+                neighbours = el.split()[2].split(',')
+                if len(neighbours) == 4:
+                    for x in range(4):
+                        nbThree = [neighbours[x%4] + ',' + neighbours[(x+1)%4] + ',' + neighbours[(x+2)%4]]
+                        # Switch the original neighbour list for the three indexes in the input line
+                        siteList.append(AcceptorInt(el.split()[:2] + nbThree + el.split()[3:], nNeighbours=4, inv=True))
+
+                else:
+                    # Set lonepairs and inverse if additional arguments
+                    input = el.split()
+                    if 'inv' in input:
+                        inverse = True
+                        input.remove('inv')
+                    else: inverse = False
+                    if len(input) > 4:
+                        siteList.append(AcceptorInt(input[:-1], lp=int(input[-1]), inv=inverse))
+                    else:
+                        siteList.append(AcceptorInt(input, inv=inverse))
+    return(siteList)
+
+
 if __name__ == '__main__':
 
 # Input file format: siteInd neighbourInds target lp(optional)
@@ -422,48 +445,32 @@ if __name__ == '__main__':
     ids = gg.atomIdentify(geomFile, numAtoms)
 
     # Create an acceptor/donor interactionSite object for each water interaction site
-    siteIDs, siteList = [], []
-    for el in input[1:]:
-        siteIDs.append(el.split()[0])
-        if el.split()[3] == 'don':
-            siteList.append(DonorInt(el.split()))
-        elif el.split()[3] == 'acc':
-            # If 4 neighbours then set up one for each face
-            neighbours = el.split()[2].split(',')
-            if len(neighbours) == 4:
-                ind = ['a', 'b', 'c', 'd']
-                for x in range(4):
-                    nbThree = [neighbours[x%4] + ',' + neighbours[(x+1)%4] + ',' + neighbours[(x+2)%4]]
-                    # Switch the original neighbour list for the three indexes in the input line
-                    siteList.append(AcceptorInt(el.split()[:2] + nbThree + el.split()[3:], extraID=ind[x]))
-            else:
-                siteList.append(AcceptorInt(el.split()))
+    siteList = inputParse(input[1:])
 
     for site in siteList:
         planar = site.localGeom(geometry)
         site.setPositions()
-#        site.writeCoords(geometry, ids, name='maxIntCoords'+ site.atomID)
-        site.zMatWriter(geometry, ids)
+        site.idealzMat()
+        site.writeOutput(geometry, ids, format='idealzMat')
 
         # Test to see if linear or planar triple bonding plane, then set up reverse position
         if planar == True:
             site.bBasis = axisRot(np.radians(180), site.bBasis[0], site.bBasis)
             site.setPositions()
-            site.fileID += '_inv'
 #            site.writeCoords(geometry, ids, name='maxIntCoords_inv'+ site.atomID)
-            site.zMatWriter(geometry, ids)
+            site.idealzMat()
+            site.writeOutput(geometry, ids, extraID='_inv', format='idealzMat')
 
         # Test to see if lone pair set up require
         if site.lonePairs == 2:
 
             site.bBasis = axisRot(np.radians(60), site.bBasis[0], site.bBasis)
             site.setPositions()
-#            site.writeCoords(geometry, ids, name='maxIntCoords_lp1'+ site.atomID)
-            site.fileID += '_lp1'
-            site.zMatWriter(geometry, ids)
+            site.idealzMat()
+            site.writeOutput(geometry, ids, extraID='_lp1', format='idealzMat')
             site.bBasis = axisRot(np.radians(-120), site.bBasis[0], site.bBasis)
             site.setPositions()
-            site.fileID = site.fileID[:-1] + '2'
-#            site.writeCoords(geometry, ids, name='maxIntCoords_lp2'+ site.atomID)
-            site.zMatWriter(geometry, ids)
+            site.idealzMat()
+            site.writeOutput(geometry, ids, extraID='_lp2', format='idealzMat')
+
 
